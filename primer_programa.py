29b0552bf25b3893b9909c8a783cadbb95acdff6
PIGNATARO <mpignataro@LA.LOGICALIS.COM>
2019-11-12 18:06:14 -0300

    mis dos primero programas python

---
+++ primer_programa.py
@@ -0,0 +1,9 @@
+
+x=5
+y=6
+x=x+y
+print(x)
+
+nombre=input("cual es tu nombre:")
+
+print(nombre)
---
+++ primer_programa_v2.py
@@ -0,0 +1,12 @@
+
+x=5
+y=6
+z=x+y
+print(x)
+print(y)
+print("z=x+y:",z)
+
+nombre=input("cual es tu nombre:")
+
+print(nombre)
+
